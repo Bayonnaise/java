91fbf4988653f126f2e84dc6cde26af89c09f7e2
David Bayon <dbayon82@googlemail.com>
2014-11-01 18:08:25 +0000

    Moon weight conversion program

---
+++ Lessons/EarthToMoonWeight.java
@@ -0,0 +1,11 @@
+// Calculate your weight on the moon.
+
+class EarthToMoonWeight {
+	public static void main(String args[]) {
+		double weight;
+
+		weight = 60;
+
+		System.out.println("A man weighing " + weight + "kg on Earth weighs " + (weight * 0.17) + "kg on the Moon.");
+	}
+}
